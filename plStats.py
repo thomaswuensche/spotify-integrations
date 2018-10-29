1842a4f930ac01cfb833b217f6dbaab66641a973
thomaswuensche <42999314+thomaswuensche@users.noreply.github.com>
2018-10-29 17:42:03 +0100

    added hs connection and used json to dump results

--- plStats.py
+++ plStats.py
@@ -1,6 +1,7 @@
 import spotipy
 import spotipy.util as util
 import credentials
+import json
 
 scope = 'playlist-read-collaborative'
 username = 't6am47' # argv
@@ -12,11 +13,16 @@ token = util.prompt_for_user_token(username, scope,
 
 if token:
     spotify = spotipy.Spotify(auth=token)
-    results = spotify.user_playlist_tracks(username,
+
+    result = spotify.user_playlist_tracks(username,
         playlist_id="spotify:user:t6am47:playlist:4doQ7lGWMlDDltEOQARV1d",
-        fields="tracks",
-        limit=30,
+        fields=None,
+        limit=2,
         offset=0,
         market="DE")
+    print(json.dumps(result, indent=4)) # dump full result (not related to spotipy request returning a JSON file. just used to print out dicts in a better way)
+    for item in result['items']:
+        track = item['track']
+        print(track['name'] + ' - ' + track['artists'][0]['name'])
 else:
     print("Can't get token for", username)
