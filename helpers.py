8e49df16aeacaa4e11d69adf773557f0ed866c21
thomaswuensche <42999314+thomaswuensche@users.noreply.github.com>
2018-12-25 02:17:35 +0100

    use object DataHandler for processing data

--- helpers.py
+++ helpers.py
@@ -2,6 +2,7 @@ import models
 import logging
 import json
 import credentials
+import database
 from exceptions import LocalTrackError
 
 def setLoggingLevel(level):
@@ -14,44 +15,51 @@ def whodunit(id):
     else :
         return "thomas"
 
-def process_data(item, spotify, connection):
-    # logging.debug(json.dumps(item, indent=4))
-    try:
-        track = get_track_data(item, spotify)
-    except LocalTrackError as e:
-        logging.warning(e)
-    else:
-        logging.debug(track.toString())
-        track.insert(connection)
+class DataHandler():
 
-def get_track_data(item, spotify):
-    track = item['track']
-    name = track['name']
+    def __init__(self, spotify, connection):
+        self.spotify = spotify
+        self.connection = connection
 
-    if item['is_local']:
-        raise LocalTrackError('LOCAL TRACK NOT IMPORTED: {}'.format(name))
+    def process_data(self, item):
+        # logging.debug(json.dumps(item, indent=4))
+        try:
+            track = self.get_track_data(item)
+        except LocalTrackError as e:
+            logging.warning(e)
+        else:
+            logging.debug(track.toString())
+            track.insert(self.connection)
 
-    champ = whodunit(item['added_by']['id'])
-    added_raw = item['added_at']
-    added_at = item['added_at'][:10]
-    added_time = added_raw[11:-1]
-    artist_result = spotify.artist(track['artists'][0]['uri'])
-    explicit = track['explicit']
-    release_date = track['album']['release_date']
-    release_date_precision = track['album']['release_date_precision']
-    duration = track['duration_ms']
-    popularity = track['popularity']
+    def get_track_data(self, item):
+        track = item['track']
+        name = track['name']
 
-    features = spotify.audio_features(track['id'])[0]
-    danceability = features['danceability']
-    energy = features['energy']
-    valence = features['valence']
-    tempo = features['tempo']
+        if item['is_local']:
+            database.increment_count_local_tracks(self.connection)
+            raise LocalTrackError('LOCAL TRACK NOT IMPORTED: {}'.format(name))
 
-    # logging.debug(json.dumps(artist_result, indent=4))
-    # logging.debug(json.dumps(features, indent=4))
+        champ = whodunit(item['added_by']['id'])
+        added_raw = item['added_at']
+        added_at = item['added_at'][:10]
+        added_time = added_raw[11:-1]
+        artist_result = self.spotify.artist(track['artists'][0]['uri'])
+        explicit = track['explicit']
+        release_date = track['album']['release_date']
+        release_date_precision = track['album']['release_date_precision']
+        duration = track['duration_ms']
+        popularity = track['popularity']
 
-    track = models.ActiveTrack(name, artist_result, champ, added_at, added_time,
-        explicit, release_date, release_date_precision, duration, popularity,
-        danceability, energy, valence, tempo)
-    return track
+        features = self.spotify.audio_features(track['id'])[0]
+        danceability = features['danceability']
+        energy = features['energy']
+        valence = features['valence']
+        tempo = features['tempo']
+
+        # logging.debug(json.dumps(artist_result, indent=4))
+        # logging.debug(json.dumps(features, indent=4))
+
+        track = models.ActiveTrack(name, artist_result, champ, added_at, added_time,
+            explicit, release_date, release_date_precision, duration, popularity,
+            danceability, energy, valence, tempo)
+        return track
