62afc27b48fa22c2f35eadd6faf4c1ee7920487f
thomaswuensche <42999314+thomaswuensche@users.noreply.github.com>
2019-10-16 17:00:23 +0200

    dry lib-coverage

--- lib-coverage.py
+++ lib-coverage.py
@@ -20,46 +20,20 @@ if token:
     api = spotipy.Spotify(auth=token)
 
     dict_lib = {}
-    list_covered_tracks = []
 
     logging.info('getting saved tracks...')
     result_lib = api.current_user_saved_tracks()
     helpers.store_result_with_date(api, dict_lib, result_lib, from_lib=True)
 
-    user_playlists = api.user_playlists(username)
-
-    logging.info('getting tracks in playlists...')
-    for playlist in user_playlists['items']:
-        if ('_' in playlist['name']) or ('//' in playlist['name']):
-            logging.info(playlist['id'] + ' - ' + playlist['name'])
-
-            result_tracks = api.user_playlist_tracks(username,
-                playlist_id=playlist['id'])
-            helpers.store_result(api, list_covered_tracks, result_tracks)
+    list_covered_tracks = helpers.store_tracks_from_playlists(api, username)
 
     diff = list(set(dict_lib.keys()) - set(list_covered_tracks))
     logging.info('tracks not covered: ' + str(len(diff)))
 
-    logging.info('sorting tracks by added at...')
-    diff_with_added_at = {}
-    for item in diff:
-        diff_with_added_at.update({item : dict_lib[item]})
+    list_upload = helpers.sort_diff_tracks(diff, dict_lib)
 
-    sorted_tuples = sorted(diff_with_added_at.items(), reverse=True, key=lambda x: x[1])
-    list_upload = []
-    for item in sorted_tuples:
-        list_upload.append(item[0])
-
-    logging.info('uploading tracks...')
-    for i in range(0, len(list_upload), 100):
-        if i == 0:
-            api.user_playlist_replace_tracks(username,
-                'spotify:user:t6am47:playlist:1D6xkvZWaikkFBCNtPT63q',
-                list_upload[i : i+100])
-        else:
-            api.user_playlist_add_tracks(username,
-                'spotify:user:t6am47:playlist:1D6xkvZWaikkFBCNtPT63q',
-                list_upload[i : i+100])
+    helpers.upload_that_shit(api, username, list_upload,
+        'spotify:user:t6am47:playlist:1D6xkvZWaikkFBCNtPT63q')
 
 else:
     logging.error("Can't get token for", username)
