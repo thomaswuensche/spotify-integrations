3007f0fdce6a45e35bd637dee08317eaf19429a3
thomaswuensche <42999314+thomaswuensche@users.noreply.github.com>
2020-11-18 17:51:47 +0100

    herokuify

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python analytics.py
--- analytics.py
+++ analytics.py
@@ -1,37 +1,16 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-from controller import AnalyticsController
 import logging
 import os
+import util
 import psycopg2 as pg
+from controller import AnalyticsController
 
-logging.basicConfig(
-    level=int(os.environ['LOG_LEVEL']),
-    format='%(levelname)s : %(funcName)s @ %(lineno)s - %(message)s'
-)
-
-username = os.environ['SPOTIFY_USERNAME']
-scope = 'playlist-read-collaborative'
-
-auth = SpotifyOAuth(
-    client_id = os.environ['CLIENT_ID'],
-    client_secret = os.environ['CLIENT_SECRET'],
-    redirect_uri = os.environ['REDIRECT_URI'],
-    scope = scope,
-    username = username,
-)
-
-api = spotipy.Spotify(auth_manager=auth)
+util.set_logging_config()
+api = util.spotipy_client()
 
 db_conn = pg.connect(os.environ['DATABASE_URL'], sslmode='require')
-logging.info('connected to db: ' + db_conn.dsn)
+logging.info(f'connected to db: {db_conn.dsn}')
 
-result = api.user_playlist_tracks(
-    username,
-    playlist_id=os.environ['PLAYLIST_HS'],
-    limit=100,
-    market="DE"
-)
+result = api.playlist_tracks(playlist_id=os.environ['PLAYLIST_HS'])
 
 controller = AnalyticsController(api, db_conn)
 controller.reset_table(os.environ['DB_TABLE'])
