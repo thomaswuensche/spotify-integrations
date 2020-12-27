ed824ccb0e8a00b4bb876e9d4c61bb770e02005d
thomaswuensche <42999314+thomaswuensche@users.noreply.github.com>
2020-12-27 18:59:58 +0100

    mv top-songs-analytics files to subdir

--- top-songs-analytics/analytics.py
+++ top-songs-analytics/analytics.py
@@ -1,9 +1,12 @@
 import logging
 import os
 import json
-import util
 import psycopg2 as pg
-from analytics_controller import AnalyticsController
+from controller import AnalyticsController
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import util
 
 util.set_logging_config()
 api = util.spotipy_client()
